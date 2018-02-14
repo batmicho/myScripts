b35774a91021cc40f1c4614f5cdeb44a3c575311
batmicho <mihaildr92ing.gmail.com>
2018-02-14 19:15:55 +0000

    log rotate check build

--- LogRotate-sh/logrotate.sh
+++ LogRotate-sh/logrotate.sh
@@ -6,14 +6,19 @@ rdygz="./logs/*.*.gz";
     echo $logs;
     echo "-----";
     echo $rdygz;
-    echo "----";
-    # echo ${rdygz[@]};
-    echo "----";
+    echo "----";  
 
-#     for b in $rdygz; 
-#         do  
-#            echo $b
-#         done
+     for b in $rdygz; 
+         do  
+            prefix="${b//[!1-9]/}"
+            #echo "$prefix" 
+            echo "------"
+            if [ -z $prefix ]; then 
+                 echo $prefix 
+             else
+                 echo "no number"
+             fi
+         done
     
 #  for i in $logs; do mv $i $i.1 && gzip $i.1 ; done
 
