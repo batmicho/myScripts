6aa4012bf8f072be8ed6d4d99a7cbbfab3f91e7f
batmicho <mihaildr92ing.gmail.com>
2018-02-20 01:40:13 +0000

    rotating 12 logs from given .log file

--- LogRotate-sh/logrotate.sh
+++ LogRotate-sh/logrotate.sh
@@ -10,18 +10,25 @@ rdygz=$(ls -r ./logs/*.*.gz);
 
     for fileName in  $rdygz; 
         do  
-             index="${fileName//[!1-9]/}"
+		index="${fileName//[^0-9]/}"
              echo "------" 
              echo "this is index: $index" 
            
-             if [ "$index" -le '11' ]; then 
+             if [ "$index" -le '9' ]; then 
                  newindex=$((index + 1)) 
                 echo "this is newindex: $newindex"
                 echo "this is fileName: $fileName"
-                cp  "$fileName" "${fileName/[0-9]/$newindex}" 
-                echo "this is new newFileName: ${fileName/[0-9]/$newindex}"
-            else
-                  echo "no number"
+		cp  "$fileName" "${fileName/[0-9]/$newindex}" 
+		echo "this is new newFileName: ${fileName/[1-9]/$newindex}"
+		
+		elif [ "$index" -le '11' ]; then
+			newindex=$((index + 1))
+			echo "this is newidex: $newindex"
+			echo "this is filename: $fileName"
+			cp "$fileName" "${fileName/[1-9][0-9]/$newindex}"
+			echo "this is newFileName: ${fileName/[1-9][0-9]/$newindex}"
+		else 
+			echo "Not a number from 1 - 12 "
               fi
          done
     
